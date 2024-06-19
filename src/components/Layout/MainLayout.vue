d0ba27b9bc0764ffdda85a7d42926575b59d86b7
yash <yash.m@peerbits.com>
2024-06-19 19:17:38 +0530

    responsive changes

--- src/components/Layout/MainLayout.vue
+++ src/components/Layout/MainLayout.vue
@@ -3,7 +3,7 @@
         <!-- Header -->
         <MainHeader />
         <!-- content -->
-        <div class="grid grid-cols-1 md:grid-cols-12 gap-10 justify-between">
+        <div class="grid grid-cols-1 md:grid-cols-12 lg:gap-10 justify-between">
             <!-- container grid grid-cols-12 md:gap-10 justify-between lg:mt-[220px] -->
             <!-- <div> -->
             <ProfileCard :profile_data="data" />
@@ -41,30 +41,12 @@
                                         </svg></span>
                                 </span>Work
                             </RouterLink>
-                            <!-- <RouterLink to="/resume" class="w-full h-20 rounded-[10px]  cursor-pointer  font-poppins  bg-[#F3F6F6]  font-medium mx-2.5  text-xtiny text-gray-lite dark:text-[#A6A6A6] justify-center flex flex-col items-center   transition-all duration-300 ease-in-out dark:hover:text-white dark:bg-[#212425] hover:text-white   hover:bg-gradient-to-r from-[#FA5252] to-[#DD2476] lg:text-white lg:dark:text-white   lg:bg-gradient-to-r from-[#FA5252] to-[#DD2476] ">
-                                <span class="text-xl mb-1">
-                                    <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                            d="M946.5 505L560.1 118.8l-25.9-25.9a31.5 31.5 0 0 0-44.4 0L77.5 505a63.9 63.9 0 0 0-18.8 46c.4 35.2 29.7 63.3 64.9 63.3h42.5V940h691.8V614.3h43.4c17.1 0 33.2-6.7 45.3-18.8a63.6 63.6 0 0 0 18.7-45.3c0-17-6.7-33.1-18.8-45.2zM568 868H456V664h112v204zm217.9-325.7V868H632V640c0-22.1-17.9-40-40-40H432c-22.1 0-40 17.9-40 40v228H238.1V542.3h-96l370-369.7 23.1 23.1L882 542.3h-96.1z">
-                                        </path>
-                                    </svg>
-                                </span>Resume
-                            </RouterLink> -->
-                            <!-- <a class="w-full h-20 rounded-[10px]  cursor-pointer  font-poppins  bg-[#F3F6F6]  font-medium mx-2.5  text-xtiny text-gray-lite dark:text-[#A6A6A6] justify-center flex flex-col items-center   transition-all duration-300 ease-in-out dark:hover:text-white dark:bg-[#212425] hover:text-white   hover:bg-gradient-to-r from-[#FA5252] to-[#DD2476] lg:text-white lg:dark:text-white   lg:bg-gradient-to-r from-[#FA5252] to-[#DD2476] " href="/home-box-layout/home">
-                                <span class="text-xl mb-1">
-                                    <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                            d="M946.5 505L560.1 118.8l-25.9-25.9a31.5 31.5 0 0 0-44.4 0L77.5 505a63.9 63.9 0 0 0-18.8 46c.4 35.2 29.7 63.3 64.9 63.3h42.5V940h691.8V614.3h43.4c17.1 0 33.2-6.7 45.3-18.8a63.6 63.6 0 0 0 18.7-45.3c0-17-6.7-33.1-18.8-45.2zM568 868H456V664h112v204zm217.9-325.7V868H632V640c0-22.1-17.9-40-40-40H432c-22.1 0-40 17.9-40 40v228H238.1V542.3h-96l370-369.7 23.1 23.1L882 542.3h-96.1z">
-                                        </path>
-                                    </svg>
-                                </span>Portfolio
-                            </a> -->
                         </ul>
                     </nav>
                 </div>
                 <div class="shadow lg:rounded-2xl bg-white dark:bg-[#111111]">
                     <div data-aos="fade" class="aos-init aos-animate">
-                        <div class="pt-12 md:py-12 px-2 sm:px-5 md:px-10 lg:px-14">
+                        <div class="p-5 md:p-8 lg:p-12">
                             <router-view />
                         </div>
                     </div>
