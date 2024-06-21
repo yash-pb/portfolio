2cd3d123d12a56d08bf7092f9c016ff59b3d92b3
yash <yash.m@peerbits.com>
2024-06-21 18:24:08 +0530

    theme change icon

--- src/components/Layout/MainHeader.vue
+++ src/components/Layout/MainHeader.vue
@@ -26,6 +26,29 @@
                     </ul>
                 </div>
                 <!-- change theme -->
+                <div class="ml-3">
+                    <div class="md:flex md:items-center md:space-x-10 theme-toggle" @click="themeToggle">
+                        <span class="bg-white w-[40px] hover:text-white h-[40px] rounded-full lg:flex justify-center items-center text-black hover:bg-[#ef4060] transition-all duration-300 ease-in-out cursor-pointer ml-2 "><svg
+                                stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24"
+                                stroke-linecap="round" stroke-linejoin="round" class="text-3xl dark-mode-light dark:hidden"
+                                height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+                                </svg><svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24"
+                                stroke-linecap="round" stroke-linejoin="round"
+                                class="text-2xl dark-mode-dark hidden fill-jacarta-700 group-hover:fill-white group-focus:fill-white dark:block dark:fill-white "
+                                height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                                <circle cx="12" cy="12" r="5"></circle>
+                                <line x1="12" y1="1" x2="12" y2="3"></line>
+                                <line x1="12" y1="21" x2="12" y2="23"></line>
+                                <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
+                                <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
+                                <line x1="1" y1="12" x2="3" y2="12"></line>
+                                <line x1="21" y1="12" x2="23" y2="12"></line>
+                                <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
+                                <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
+                            </svg></span>
+                    </div> 
+                </div>
             </nav>
         </div>
     </header>
@@ -38,7 +61,6 @@
             </li>
         </ul>
     </div>
-    <!-- <button @click="themeToggle" class="btn dark:text-white">Change</button> -->
 </template>
 <script setup>
 import { RouterLink } from "vue-router";
