0e7847adcdb16d2de9bd5684c286d4a9c6b71fcc
yash <yash.m@peerbits.com>
2024-06-21 16:55:48 +0530

    update icon and navbar in layout

--- src/components/Layout/MainLayout.vue
+++ src/components/Layout/MainLayout.vue
@@ -11,18 +11,12 @@
             <div class="col-span-12 lg:col-span-8 mt-8">
                 <div class="shadow lg:w-[526px] h-[144px] hidden lg:block p-[30px] ml-auto mb-5 rounded-[16px] bg-white dark:bg-[#111111] ">
                     <nav class="hidden lg:block">
-                        <ul class="flex">
-                            <RouterLink to="/"
-                                class="router-link w-full h-20 rounded-[10px]  cursor-pointer  font-poppins  bg-[#F3F6F6]  font-medium mx-2.5  text-xtiny text-gray-lite dark:text-[#A6A6A6] justify-center flex flex-col items-center   transition-all duration-300 ease-in-out dark:hover:text-white dark:bg-[#212425]">
-                                <span class="text-xl mb-1">
-                                    <svg stroke="currentColor" fill="currentColor" stroke-width="2" viewBox="0 0 1024 1024" height="1.2em" width="1.2em" stroke-linecap="round" stroke-linejoin="round" xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                            d="M946.5 505L560.1 118.8l-25.9-25.9a31.5 31.5 0 0 0-44.4 0L77.5 505a63.9 63.9 0 0 0-18.8 46c.4 35.2 29.7 63.3 64.9 63.3h42.5V940h691.8V614.3h43.4c17.1 0 33.2-6.7 45.3-18.8a63.6 63.6 0 0 0 18.7-45.3c0-17-6.7-33.1-18.8-45.2zM568 868H456V664h112v204zm217.9-325.7V868H632V640c0-22.1-17.9-40-40-40H432c-22.1 0-40 17.9-40 40v228H238.1V542.3h-96l370-369.7 23.1 23.1L882 542.3h-96.1z">
-                                        </path>
-                                    </svg>
-                                </span>Home
+                        <ul class="flex" v-if="data.navbar">
+                            <RouterLink v-for="(navbar, index) in data.navbar" :key="index" :to="navbar.url" class="router-link w-full h-20 rounded-[10px]  cursor-pointer  font-poppins  bg-[#F3F6F6]  font-medium mx-2.5  text-xtiny text-gray-lite dark:text-[#A6A6A6] justify-center flex flex-col items-center   transition-all duration-300 ease-in-out dark:hover:text-white dark:bg-[#212425]">
+                                <span class="text-xl mb-1" v-html="navbar.icon">
+                                </span>{{ navbar.title }}
                             </RouterLink>
-                            <RouterLink to="/work"
+                            <!-- <RouterLink to="/work"
                                 class="router-link w-full h-20 rounded-[10px]  cursor-pointer  font-poppins  bg-[#F3F6F6]  font-medium mx-2.5  text-xtiny text-gray-lite dark:text-[#A6A6A6] justify-center flex flex-col items-center   transition-all duration-300 ease-in-out dark:hover:text-white dark:bg-[#212425]">
                                 <span class="text-xl mb-1">
                                     <span class="text-xl mb-1">
@@ -48,7 +42,7 @@
                                         </svg>
                                     </span>
                                 </span>Projects
-                            </RouterLink>
+                            </RouterLink> -->
                         </ul>
                     </nav>
                 </div>
